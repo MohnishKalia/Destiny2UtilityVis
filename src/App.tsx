ee454f218b03a7bc0c11e1a1e2b0da5c8c8f2cba
Mohnish Kalia <mohnish.kalia@gmail.com>
2021-01-17 04:28:33 -0600

    Working item filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { useState, useEffect } from 'react';
-import { DestinyInventoryItemDefinition, getDestinyManifest, getDestinyManifestSlice, HttpClient, HttpClientConfig } from 'bungie-api-ts/destiny2';
+import { DestinyInventoryItemDefinition, getDestinyManifest, getDestinyManifestSlice, HttpClient, HttpClientConfig, TierType } from 'bungie-api-ts/destiny2';
 import RJW from 'react-json-view';
 import './App.css';
 
 const $http: HttpClient = async (config: HttpClientConfig) => {
   // console.log(process.env.REACT_APP_API_KEY);
   const res = await fetch(
-    // `https://cors-anywhere.herokuapp.com/${config.url}`
-    config.url
+    `https://cors-anywhere.herokuapp.com/${config.url}`
+    // config.url
     , {
-    headers: {
-      'X-API-Key': process.env.REACT_APP_API_KEY!,
-    }
-  });
+      headers: {
+        'X-API-Key': process.env.REACT_APP_API_KEY!,
+      }
+    });
   return res.json();
 }
 
@@ -24,22 +24,36 @@ function App() {
       const destinyManifest = await getDestinyManifest($http);
       const manifestTables = await getDestinyManifestSlice($http, {
         destinyManifest: destinyManifest.Response,
-        tableNames: ['DestinyInventoryItemDefinition', 'DestinyItemCategoryDefinition'],
+        tableNames: ['DestinyInventoryItemDefinition', 'DestinyItemCategoryDefinition', 'DestinyPowerCapDefinition'],
         language: 'en'
       });
 
       const categories = [
-        // "Armor",
+        "Armor",
         "Weapon",
       ] as const
 
-      const payload: { [key: string]: DestinyInventoryItemDefinition | undefined } = {};
+      const tiers = [
+        5, // legendary
+        6, // exotic
+      ];
+
+      const payload: { [key: string]: any } = {};
 
       for (const cat of categories) {
         const hash = Object.values(manifestTables.DestinyItemCategoryDefinition).find(def => def.shortTitle === cat)?.hash;
         if (hash)
-          payload[cat] = Object.values(manifestTables.DestinyInventoryItemDefinition).find(def => def.itemCategoryHashes?.includes(hash));
+          payload[cat] = Object.values(manifestTables.DestinyInventoryItemDefinition)
+            .filter(def => def.itemCategoryHashes?.includes(hash) && tiers.includes(def.inventory?.tierType ?? 0))
+            .map(def => ({
+              name: def.displayProperties.name,
+              description: def.displayProperties.description,
+              quality: def.quality?.versions.map(ver => manifestTables.DestinyPowerCapDefinition[ver.powerCapHash].powerCap),
+              tierType: def.inventory?.tierTypeName,
+            }));
       }
+
+
 
       setItems(payload);
     }
