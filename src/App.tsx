385104bbde66d0162aab77fe1fff13543a208d2a
Mohnish Kalia <mohnish.kalia@gmail.com>
2021-01-17 03:40:03 -0600

    Initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,56 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { DestinyInventoryItemDefinition, getDestinyManifest, getDestinyManifestSlice, HttpClient, HttpClientConfig } from 'bungie-api-ts/destiny2';
+import RJW from 'react-json-view';
 import './App.css';
 
+const $http: HttpClient = async (config: HttpClientConfig) => {
+  // console.log(process.env.REACT_APP_API_KEY);
+  const res = await fetch(
+    // `https://cors-anywhere.herokuapp.com/${config.url}`
+    config.url
+    , {
+    headers: {
+      'X-API-Key': process.env.REACT_APP_API_KEY!,
+    }
+  });
+  return res.json();
+}
+
 function App() {
+  const [items, setItems] = useState({});
+
+  useEffect(() => {
+    async function getData() {
+      const destinyManifest = await getDestinyManifest($http);
+      const manifestTables = await getDestinyManifestSlice($http, {
+        destinyManifest: destinyManifest.Response,
+        tableNames: ['DestinyInventoryItemDefinition', 'DestinyItemCategoryDefinition'],
+        language: 'en'
+      });
+
+      const categories = [
+        // "Armor",
+        "Weapon",
+      ] as const
+
+      const payload: { [key: string]: DestinyInventoryItemDefinition | undefined } = {};
+
+      for (const cat of categories) {
+        const hash = Object.values(manifestTables.DestinyItemCategoryDefinition).find(def => def.shortTitle === cat)?.hash;
+        if (hash)
+          payload[cat] = Object.values(manifestTables.DestinyInventoryItemDefinition).find(def => def.itemCategoryHashes?.includes(hash));
+      }
+
+      setItems(payload);
+    }
+
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RJW src={items} />
+    </>
   );
 }
 
